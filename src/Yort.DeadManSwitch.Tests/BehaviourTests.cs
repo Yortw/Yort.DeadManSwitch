01620f65fe63ae70a68d8072ea2f43c9b5857e65
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2024-11-07 20:01:41 +1300

    Rename test namespace.

--- src/Yort.DeadManSwitch.Tests/BehaviourTests.cs
+++ src/Yort.DeadManSwitch.Tests/BehaviourTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Yort.Dms.Shared.Tests
+namespace Yort.Dms.Tests
 {
 	[TestClass]
 	public class BehaviourTests
